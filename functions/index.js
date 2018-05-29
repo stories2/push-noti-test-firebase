b47a02f6a53a13bd7f624b1ad3d5a43b0d764bd0
Kim HyunWoo <stories2@naver.com>
2018-05-30 00:09:15 +0900

    index module : require admin

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest(function(request, response) {
-//     response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
 
 const express = require('express');
 const cors = require('cors')({origin: true});
