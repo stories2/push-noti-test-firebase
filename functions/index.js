2a90c4eb3e1db5d68a260b6a7f87422a1fb69454
Kim HyunWoo <stories2@naver.com>
2018-05-30 00:06:24 +0900

    Project setting <init template>

---
+++ functions/index.js
@@ -0,0 +1,55 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest(function(request, response) {
+//     response.send("Hello from Firebase!");
+// });
+
+const express = require('express');
+const cors = require('cors')({origin: true});
+const app = express();
+
+app.use(cors)
+
+app.get("/keyboard", function (request, response) {
+    responseMessage = {
+        "type" : "text"
+    }
+
+    // response.setHeader('Content-Type', 'application/json');
+    response.status(200).send(JSON.stringify(responseMessage))
+})
+
+app.post("/message", function (request, response) {
+
+    console.log("/message-> user_key: " + request.body["user_key"])
+    responseMessage = {
+        "message": {
+            "text": request.body["content"],
+        }
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).send(JSON.stringify(responseMessage))
+})
+
+app.post("/friend", function (request, response) {
+
+    console.log("/friend-> user_key: " + request.body["user_key"])
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).send()
+})
+
+app.delete("/friend/:user_key", function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).send()
+})
+
+app.delete("/chat_room/:user_key", function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).send()
+})
+
+exports.v1 = functions.https.onRequest(app);
