7c2332462b2bb807018d34061c2f0a19426950ee
Kim HyunWoo <stories2@naver.com>
2018-10-27 19:37:02 +0900

    App : add route, cache cleaner, spinner

--- public/assets/js/App.js
+++ public/assets/js/App.js
@@ -5,3 +5,57 @@ var app = angular.module('ADSModule', ['ngCookies',
     'ngAnimate',
     'ngAria',
     'ngMaterial']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "/static/partials/index.html",
+            controller : "indexController",
+            cache: false,
+            disableCache: true,
+        })
+        .when("/chapel", {
+            templateUrl : "../static/partials/chapel/chapelReport.html",
+            // controller : "chapelReportController"
+            cache: false,
+            disableCache: true,
+        })
+        .when("/score", {
+            templateUrl : "../static/partials/student/scoreReport.html",
+            // controller : "chapelReportController"
+            cache: false,
+            disableCache: true,
+        }).otherwise({
+        redirectTo: '/',
+        cache: false,
+        disableCache: true,
+    });
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push(function ($q, $rootScope) {
+        return {
+            'request': function (config) {
+                // intercepts the request
+                $('#spinnerView').show();
+                return config;
+            },
+            'response': function (response) {
+                // intercepts the response. you can examine things like status codes
+                $('#spinnerView').hide();
+                return response;
+            },
+            'responseError': function (response) {
+                // intercepts the response when the response was an error
+                $('#spinnerView').hide();
+                return response;
+            }
+        };
+    });
+});
+
+app.run(function($rootScope, $templateCache) {
+    $rootScope.$on('$viewContentLoaded', function() {
+        $templateCache.removeAll();
+    });
+});
