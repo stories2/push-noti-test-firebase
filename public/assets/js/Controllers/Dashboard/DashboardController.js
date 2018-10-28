ca51d03bcc4edf1263a44eea19ed3f16da6d6042
Kim HyunWoo <stories2@naver.com>
2018-10-28 14:58:56 +0900

    DashboardController : check user is signed in

--- public/assets/js/Controllers/Dashboard/DashboardController.js
+++ public/assets/js/Controllers/Dashboard/DashboardController.js
@@ -1,3 +1,25 @@
 app.controller("DashboardController", function ($scope, $http, $mdToast, $mdSidenav, ADSModuleService) {
     ADSModuleService.printLogMessage("DashboardController", "DashboardController", "init", LOG_LEVEL_INFO);
+
+    $scope.listenAuthStateChanged = function () {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                ADSModuleService.printLogMessage("DashboardController", "listenAuthStateChanged", "user signed in: " + JSON.stringify(user), LOG_LEVEL_DEBUG)
+                LoadLatestDeployStatus()
+
+                // ...
+            } else {
+                // User is signed out.
+                // ...
+                ADSModuleService.printLogMessage("DashboardController", "listenAuthStateChanged", "user not signed in", LOG_LEVEL_WARN)
+
+                
+            }
+        });
+    }
+    
+    function LoadLatestDeployStatus() {
+        ADSModuleService.printLogMessage("DashboardController", "LoadLatestDeployStatus", "load status db", LOG_LEVEL_INFO)
+    }
 });
