2a5a8c7cabed1d517a69248f42d1a13b9acfcc63
Kim HyunWoo <stories2@naver.com>
2018-10-28 17:18:45 +0900

    DeployController : open publish dialog

--- public/assets/js/Controllers/Dashboard/DeployController.js
+++ public/assets/js/Controllers/Dashboard/DeployController.js
@@ -4,5 +4,39 @@ app.controller("DeployController", function ($scope, $http, $mdToast, $mdDialog,
 
     $scope.onBtnPublishClicked = function () {
         ADSModuleService.printLogMessage("DeployController", "onBtnPublishClicked", "make new publish", LOG_LEVEL_INFO)
+        ShowDeployRequestDialog()
+    }
+
+    function ShowDeployRequestDialog() {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'templates/dialog/DeployRequest.html',
+            parent: angular.element(document.body),
+            // targetEvent: ev,
+            clickOutsideToClose:true,
+            locals: {
+            },
+            // fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        })
+            .then(function(answer) {
+                // $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                // $scope.status = 'You cancelled the dialog.';
+            });
+    }
+
+    function DialogController($scope, $mdDialog) {
+
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
     }
 })
