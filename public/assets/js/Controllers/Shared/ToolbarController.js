6d188daa1ecac4bb6c1fc6fac8f1589adfd3c6be
Kim HyunWoo <stories2@naver.com>
2018-10-28 10:30:40 +0900

    ToolbarController : listen user signed state changed

--- public/assets/js/Controllers/Shared/ToolbarController.js
+++ public/assets/js/Controllers/Shared/ToolbarController.js
@@ -4,4 +4,18 @@ app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidena
     $scope.isUserSignedIn = false;
 
     ADSModuleService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO);
+
+    $scope.listenAuthStateChanged = function () {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                ADSModuleService.printLogMessage("ToolbarController", "listenAuthStateChanged", "user signed in: " + JSON.stringify(user), LOG_LEVEL_DEBUG)
+                // ...
+            } else {
+                // User is signed out.
+                // ...
+                ADSModuleService.printLogMessage("ToolbarController", "listenAuthStateChanged", "user not signed in", LOG_LEVEL_WARN)
+            }
+        });
+    }
 });
