fc5a2e4516124733112e82f79aaebfb1cd48cfad
Kim HyunWoo <stories2@naver.com>
2018-10-28 10:16:38 +0900

    ToolbarController : show icon is user signed in

--- public/assets/js/Controllers/Shared/ToolbarController.js
+++ public/assets/js/Controllers/Shared/ToolbarController.js
@@ -1,4 +1,7 @@
 app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidenav, ADSModuleService) {
+
     $scope.title = "ADS Console";
+    $scope.isUserSignedIn = false;
+
     ADSModuleService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO);
 });
