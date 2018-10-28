e5e3089e779162585d37441245b9b680193443e4
Kim HyunWoo <stories2@naver.com>
2018-10-28 10:45:39 +0900

    LoginController : listen user auth state

--- public/assets/js/Controllers/Dialog/LoginController.js
+++ public/assets/js/Controllers/Dialog/LoginController.js
@@ -1,3 +1,20 @@
 app.controller("LoginController", function ($scope, $http, $mdToast, $mdSidenav, ADSModuleService) {
+
+    $scope.isUserSignedIn = false;
+
     ADSModuleService.printLogMessage("LoginController", "LoginController", "init", LOG_LEVEL_INFO);
+
+    $scope.listenAuthStateChanged = function () {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                ADSModuleService.printLogMessage("LoginController", "listenAuthStateChanged", "user signed in: " + JSON.stringify(user), LOG_LEVEL_DEBUG)
+                // ...
+            } else {
+                // User is signed out.
+                // ...
+                ADSModuleService.printLogMessage("LoginController", "listenAuthStateChanged", "user not signed in", LOG_LEVEL_WARN)
+            }
+        });
+    }
 });
